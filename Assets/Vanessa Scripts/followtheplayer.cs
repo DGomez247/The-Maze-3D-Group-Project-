c327df61fd60fcf4393d25c8f66263129ab1ef32
vanessa <vvanessachammas@gmail.com>
2020-04-21 13:50:35 -0700

    nav mesh for enemy AI

---
+++ Assets/Vanessa Scripts/followtheplayer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class followtheplayer : MonoBehaviour
+{
+
+
+    public GameObject target = null;
+    private NavMeshAgent nma = null;
+
+    // Start is called before the first frame update
+    public void Start()
+    {
+        nma = this.GetComponent<NavMeshAgent>();
+
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        nma.SetDestination(target.transform.position);
+    }
+
+}
