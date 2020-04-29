873c5a53431e4e2fff32427b58b6be85e79c6e6b
Carter <andrewscart@gmail.com>
2020-04-28 17:40:59 -0700

    Merge branch 'master' of https://rijeka.sdsu.edu/cs59620s_03/cs596_s20_3d

--- Assets/Vanessa Scripts/followtheplayer.cs
+++ Assets/Vanessa Scripts/followtheplayer.cs
@@ -2,14 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
-
+using UnityEngine.SceneManagement;
 public class followtheplayer : MonoBehaviour
 {
 
 
     public GameObject target = null;
     private NavMeshAgent nma = null;
-
+    bool resting = false;
+    public float maxBites;
+    public AudioSource biteSound;
+    float biteCound = 0;
     // Start is called before the first frame update
     public void Start()
     {
@@ -20,7 +23,27 @@ public class followtheplayer : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
+        if(!resting)
         nma.SetDestination(target.transform.position);
     }
-
+    private void OnCollisionEnter(Collision collision)
+    {
+        print("collision");
+        if (collision.collider.CompareTag("Player")&&!resting)
+        {
+            StartCoroutine(bite());
+        }
+    }
+    private IEnumerator bite()
+    {
+        biteSound.Play();
+        biteCound++;
+        resting = true;
+        yield return new WaitForSeconds(1);
+        if (biteCound == maxBites)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+        resting = false;
+    }
 }
