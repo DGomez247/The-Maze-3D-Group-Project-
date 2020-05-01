ed126c17acc6bfc5fddb72eebba7bc1c3762381a
DGomez247 <44910412+DGomez247@users.noreply.github.com>
2020-05-01 14:04:44 -0700

    Some PGC Implemented
    Eastern Walls will change in the maze each time the game starts

---
+++ Assets/Scripts/Diego's Scripts/PCGMaze.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PCGMaze : MonoBehaviour
+{
+
+    public Transform[] spawnpoints;
+    public GameObject Wall;
+
+
+    void Start()
+    {
+        spawnWall();
+    }
+
+    public void spawnWall()
+    {
+        int spawnPoint = Random.Range(0, spawnpoints.Length);
+        Instantiate(Wall, spawnpoints[spawnPoint].position, spawnpoints[spawnPoint].rotation);
+    }
+}
