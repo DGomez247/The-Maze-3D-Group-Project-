8dcd6cef13275749fb5953c124fff7c10d21055f
DGomez247 <44910412+DGomez247@users.noreply.github.com>
2020-05-01 13:05:07 -0700

    Made Exit Trigger
    Collider at the end of the Hallway makes it go to the credits

---
+++ Assets/Scripts/Diego's Scripts/ExitTrigger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ExitTrigger : MonoBehaviour
+{
+   
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene(2);
+        }
+    }
+
+
+}
