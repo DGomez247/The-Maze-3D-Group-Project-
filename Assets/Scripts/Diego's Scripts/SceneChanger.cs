551c126124e98eb66eeeaabfdf3db9e03600b790
DGomez247 <44910412+DGomez247@users.noreply.github.com>
2020-05-01 14:56:52 -0700

    Control Scene
    Added Control Scene to show the controls to the game

--- Assets/Scripts/Diego's Scripts/SceneChanger.cs
+++ Assets/Scripts/Diego's Scripts/SceneChanger.cs
@@ -5,6 +5,10 @@ using UnityEngine.SceneManagement;
 
 public class SceneChanger : MonoBehaviour
 {
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
    public void LoadGame()
     {
         SceneManager.LoadScene(1);
