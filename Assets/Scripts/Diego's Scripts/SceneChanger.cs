44e457b6225d4718ed49dfe69e318246f40e952e
DGomez247 <44910412+DGomez247@users.noreply.github.com>
2020-05-01 12:45:11 -0700

    Simple Menu and Credits
    Simple menu and credits to build upon later in the build.

---
+++ Assets/Scripts/Diego's Scripts/SceneChanger.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+   public void LoadGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void LoadCredits()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void loadControls()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void LoadMultiplayer()
+    {
+        SceneManager.LoadScene(4);
+    }
+
+    public void Quitgame()
+    {
+
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
